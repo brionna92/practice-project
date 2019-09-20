8adee56f073be9467f831977ef5327e2a453acc6
Brionna Anderson <andersonbrionna@gmail.com>
2019-09-19 21:16:12 -0500

    add name to project

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function greeter (question = "What is you name?"){
+return prompt (question);
+}
+
+const answer = greeter();
+document.querySelector("").textContent = answer
+
+const h1TextContent = document.querySelector("h1")textContent;
+console.log(h1TextContent);
