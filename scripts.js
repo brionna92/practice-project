e26a5f9916d454ab117f696f1a77568ce9e1b5d9
Brionna Anderson <andersonbrionna@gmail.com>
2019-09-24 21:13:06 -0500

    add js to toggle nav menu

--- scripts.js
+++ scripts.js
@@ -2,10 +2,11 @@
 // Listen for a click on the hamburger
 // remove mobile hidden class from the ul
 
-const hamburger = document.querySelector("fa-hamburger");
+const hamburger = document.querySelector(".fa-hamburger");
 const hiddenUL = document.querySelector("ul");
 console.log(hamburger, hiddenUL);
 
-hamburger.addEventListener(`click`, function(){
-console.log("hello from hamburger");
+hamburger.addEventListener("click", function(){
+  //hiddenUL.classList.remove("is-hidden--mobile");
+  hiddenUL.classList.toggle("is-hidden--mobile");
 });
