670e279fa7b2af46f2afec961bdbb807c15f2092
Brionna Anderson <andersonbrionna@gmail.com>
2019-10-01 20:04:46 -0500

    created functions inside of components heading

--- index.js
+++ index.js
@@ -22,18 +22,24 @@ Currently #root div is empty
 We want to grb the #root div
 We want tko assign the markup that is contained in the components*/
 
+
+// st represents a piece of states
+function render( st = state.home){
 document.querySelector("#root").innerHTML = `
-${Header(state.home.heading)}
+${Header(st.heading)}
 ${Nav()}
 ${Main()}
 ${Footer()}
-`
+`;
+}
+
+render()
 
 const aboutLink = document.querySelector('#about');
 
 aboutLink.addEventListener('click', function (event){
 event.preventDefault();
-console.log(state[event.target. textContent]);
+render(state[event.target. textContent]);
 
-  console.log('you clicked me')
+
 })
